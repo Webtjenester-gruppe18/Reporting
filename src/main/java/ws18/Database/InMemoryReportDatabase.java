9e615e3c737435688a7916aa169cc349de51f3ba
AM <lgv2800@gmail.com>
2020-01-16 11:07:25 +0100

    Added InMemoryReportDatabase to database folder.

---
+++ src/main/java/ws18/Database/InMemoryReportDatabase.java
@@ -0,0 +1,33 @@
+package ws18.Database;
+
+import Model.DTUPayTransaction;
+
+import java.util.ArrayList;
+
+public class InMemoryReportDatabase implements IReportDatabase {
+
+    private ArrayList<DTUPayTransaction> transactions = new ArrayList<>();
+
+    @Override
+    public DTUPayTransaction getTransactionById(String transactionId) {
+        for (DTUPayTransaction transaction : this.transactions) {
+            if (transaction.getTransactionId().equals(transactionId)) {
+                return transaction;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<DTUPayTransaction> getAllTransactions() {
+        return this.transactions;
+    }
+
+    @Override
+    public String saveTransaction(DTUPayTransaction transaction) {
+        this.transactions.add(transaction);
+
+        return transaction.getTransactionId();
+    }
+}
