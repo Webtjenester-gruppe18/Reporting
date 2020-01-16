1231cb8f56c68684b2876a060aa2c35f7a8b2d0a
AM <lgv2800@gmail.com>
2020-01-16 12:22:08 +0100

    Changed ReportingController. Added endpoint.

--- src/main/java/ws18/Controller/ReportingController.java
+++ src/main/java/ws18/Controller/ReportingController.java
@@ -1,15 +1,32 @@
 package ws18.Controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ws18.Control.ControlReg;
+import ws18.Model.DTUPayTransaction;
+import ws18.Service.IReportingService;
+
+import java.util.ArrayList;
 
 @RestController
 public class ReportingController {
 
-    @GetMapping("/report")
+    private IReportingService reportingService = ControlReg.getReportingService();
+
+    @RequestMapping("/report")
     public String index() {
         return "Greetings from report endpoint";
     }
+
+    @RequestMapping(path = "/transactions", method = RequestMethod.GET)
+    public ArrayList<DTUPayTransaction> getAllTransactions() {
+        return this.reportingService.getAllTransactions();
+    }
+
+    /*
+    @RequestMapping(path = "/report/customer/{}")
+    public ResponseEntity<Object> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        //return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }*/
 
 }
