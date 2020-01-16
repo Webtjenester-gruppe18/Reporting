75f9d20d12726cb9f6d7a4064128361e2d33304e
AM <lgv2800@gmail.com>
2020-01-16 14:55:47 +0100

    Changed DateTimeHelper.

--- src/main/java/ws18/Helper/DateTimeHelper.java
+++ src/main/java/ws18/Helper/DateTimeHelper.java
@@ -6,6 +6,9 @@ import java.util.Date;
 
 public class DateTimeHelper {
 
+    public static final long DAY_IN_MILLIS = 86400;
+    public static final long MONTH_IN_MILLIS = DAY_IN_MILLIS * 30;
+
     public static long convertDateStringToMS(String dateFrom) {
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMMyyyy");
