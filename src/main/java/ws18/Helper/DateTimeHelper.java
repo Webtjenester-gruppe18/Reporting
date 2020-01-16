1d8ba46349112ab7654508d5612073c9a714073a
AM <lgv2800@gmail.com>
2020-01-16 14:28:43 +0100

    Added DateTimeHelper.

---
+++ src/main/java/ws18/Helper/DateTimeHelper.java
@@ -0,0 +1,24 @@
+package ws18.Helper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTimeHelper {
+
+    public long convertDateStringToMS(String dateFrom) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMMyyyy");
+        long dateFromInMS = 0;
+
+        try {
+            Date date = simpleDateFormat.parse(dateFrom);
+            dateFromInMS = date.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return dateFromInMS;
+    }
+
+}
