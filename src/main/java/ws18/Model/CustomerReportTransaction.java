e5815ad2702a705a50458f1318c0acf6a73bd142
AM <lgv2800@gmail.com>
2020-01-16 11:16:08 +0100

    Added Customer, CustomerReportTransaction and DTUPayUser into model.

---
+++ src/main/java/ws18/Model/CustomerReportTransaction.java
@@ -0,0 +1,32 @@
+package ws18.Model;
+
+import java.math.BigDecimal;
+
+public class CustomerReportTransaction extends ReportTransaction {
+
+    private String merchantAccountId;
+
+    public CustomerReportTransaction(BigDecimal amount, String description, long time, Token token, String merchantAccountId) {
+        super(amount, description, time, token);
+
+        this.merchantAccountId = merchantAccountId;
+    }
+
+    public CustomerReportTransaction(DTUPayTransaction transaction, Customer customer) {
+        super(transaction.getAmount(), transaction.getDescription(), transaction.getTime(), transaction.getToken());
+
+        if (customer.getAccountId().equals(transaction.getCreditor())) {
+            this.merchantAccountId = transaction.getDebtor();
+        } else {
+            this.merchantAccountId = transaction.getCreditor();
+        }
+    }
+
+    public String getMerchantAccountId() {
+        return merchantAccountId;
+    }
+
+    public void setMerchantAccountId(String merchantAccountId) {
+        this.merchantAccountId = merchantAccountId;
+    }
+}
