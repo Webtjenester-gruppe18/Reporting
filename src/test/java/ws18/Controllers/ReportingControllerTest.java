c5f14ea1bc2b1c4745c577a495fb52c2f87f29f3
AM <lgv2800@gmail.com>
2020-01-16 15:36:06 +0100

    Added ReportingControllerTest, tests for ReportingController.

---
+++ src/test/java/ws18/Controllers/ReportingControllerTest.java
@@ -0,0 +1,63 @@
+package ws18.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ws18.Controller.ReportingController;
+import ws18.Helper.DateTimeHelper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ReportingControllerTest {
+
+    private ReportingController reportingController = new ReportingController();
+    private String customerCpr = "101097-0101";
+    private String merchantCpr = "202097-0202";
+    private String transactionId = "a7436627-ab02-4ecd-9162-43f610cbc427";
+    private long dateFrom = DateTimeHelper.MONTH_IN_MILLIS;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllTransactions() throws Exception {
+        mockMvc.perform(get("/transactions"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getTransactionById() throws Exception {
+        mockMvc.perform(get("/transactions/" + transactionId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getCustomerTransactionsByIds() throws Exception {
+        mockMvc.perform(get("/transactions/customer/" + customerCpr))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getMerchantTransactionsByIds() throws Exception {
+        mockMvc.perform(get("/transactions/merchant/" + merchantCpr))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getCustomerTransactionsByIdsFromThenToNow() throws Exception {
+        mockMvc.perform(get("/transactions/customer/" + customerCpr + "/" + dateFrom))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getMerchantTransactionsByIdsFromThenToNow() throws Exception {
+        mockMvc.perform(get("/transactions/merchant/" + merchantCpr + "/" + dateFrom))
+                .andExpect(status().isOk());
+    }
+
+}
