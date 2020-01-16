fb87211eef4822aa67ae8fd4790b0c62d8edff62
AM <lgv2800@gmail.com>
2020-01-16 11:14:35 +0100

    Added IReportingService and ReportingService in service folder.

---
+++ src/main/java/ws18/Service/IReportingService.java
@@ -0,0 +1,17 @@
+package ws18.Service;
+
+import Model.*;
+
+import java.util.ArrayList;
+
+public interface IReportingService {
+
+    DTUPayTransaction getTransactionById(String transactionId);
+    ArrayList<CustomerReportTransaction> getCustomerTransactionsByIds(Customer customer);
+    ArrayList<CustomerReportTransaction> getCustomerTransactionsByIdsFromThenToNow(Customer customer, long fromTime);
+    ArrayList<MerchantReportTransaction> getMerchantTransactionsByIds(Merchant merchant);
+    ArrayList<MerchantReportTransaction> getMerchantTransactionsByIdsFromThenToNow(Merchant merchant, long fromTime);
+    ArrayList<DTUPayTransaction> getAllTransactions();
+    String saveTransaction(DTUPayTransaction transaction);
+
+}
---
+++ src/main/java/ws18/Service/ReportingService.java
@@ -0,0 +1,107 @@
+package ws18.Service;
+
+import Control.ControlReg;
+import Database.IReportDatabase;
+import Model.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReportingService implements IReportingService {
+
+    private IReportDatabase reportDatabase = ControlReg.getReportDatabase();
+
+    @Override
+    public DTUPayTransaction getTransactionById(String transactionId) {
+        return this.reportDatabase.getTransactionById(transactionId);
+    }
+
+    @Override
+    public ArrayList<CustomerReportTransaction> getCustomerTransactionsByIds(Customer customer) {
+
+        ArrayList<CustomerReportTransaction> result = new ArrayList<>();
+
+        for (String transactionId : customer.getTransactionIds()) {
+            DTUPayTransaction transaction = this.reportDatabase.getTransactionById(transactionId);
+
+            CustomerReportTransaction reportTransaction =
+                    new CustomerReportTransaction(transaction, customer);
+
+            result.add(reportTransaction);
+        }
+
+        return result;
+    }
+
+    @Override
+    public ArrayList<CustomerReportTransaction> getCustomerTransactionsByIdsFromThenToNow(Customer customer, long fromTime) {
+
+        ArrayList<CustomerReportTransaction> result = new ArrayList<>();
+
+        for (String transactionId : customer.getTransactionIds()) {
+            DTUPayTransaction transaction = this.reportDatabase.getTransactionById(transactionId);
+
+            if (transaction.getTime() > new Date().getTime() - fromTime) {
+                CustomerReportTransaction reportTransaction =
+                        new CustomerReportTransaction(transaction, customer);
+
+                result.add(reportTransaction);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public ArrayList<MerchantReportTransaction> getMerchantTransactionsByIds(Merchant merchant) {
+
+        ArrayList<MerchantReportTransaction> result = new ArrayList<>();
+
+        for (String transactionId : merchant.getTransactionIds()) {
+            DTUPayTransaction transaction = this.reportDatabase.getTransactionById(transactionId);
+
+            MerchantReportTransaction merchantReportTransaction =
+                    new MerchantReportTransaction(
+                            transaction.getAmount(),
+                            transaction.getDescription(),
+                            transaction.getTime(),
+                            transaction.getToken());
+
+            result.add(merchantReportTransaction);
+        }
+
+        return result;
+    }
+
+    @Override
+    public ArrayList<MerchantReportTransaction> getMerchantTransactionsByIdsFromThenToNow(Merchant merchant, long fromTime) {
+        ArrayList<MerchantReportTransaction> result = new ArrayList<>();
+
+        for (String transactionId : merchant.getTransactionIds()) {
+            DTUPayTransaction transaction = this.reportDatabase.getTransactionById(transactionId);
+
+            if (transaction.getTime() > new Date().getTime() - fromTime) {
+                MerchantReportTransaction merchantReportTransaction =
+                        new MerchantReportTransaction(
+                                transaction.getAmount(),
+                                transaction.getDescription(),
+                                transaction.getTime(),
+                                transaction.getToken());
+
+                result.add(merchantReportTransaction);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public ArrayList<DTUPayTransaction> getAllTransactions() {
+        return this.reportDatabase.getAllTransactions();
+    }
+
+    @Override
+    public String saveTransaction(DTUPayTransaction transaction) {
+        return this.reportDatabase.saveTransaction(transaction);
+    }
+}
