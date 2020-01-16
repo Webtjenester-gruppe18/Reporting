93ffe5d989f39abddb8042deab79d01e6586f257
AM <lgv2800@gmail.com>
2020-01-16 15:14:53 +0100

    Changed DateTimeHelper. Removed conversion from datestring to ms.

--- src/main/java/ws18/Helper/DateTimeHelper.java
+++ src/main/java/ws18/Helper/DateTimeHelper.java
@@ -1,27 +1,8 @@
 package ws18.Helper;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class DateTimeHelper {
 
     public static final long DAY_IN_MILLIS = 86400;
     public static final long MONTH_IN_MILLIS = DAY_IN_MILLIS * 30;
-
-    public static long convertDateStringToMS(String dateFrom) {
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMMyyyy");
-        long dateFromInMS = 0;
-
-        try {
-            Date date = simpleDateFormat.parse(dateFrom);
-            dateFromInMS = date.getTime();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return dateFromInMS;
-    }
 
 }
