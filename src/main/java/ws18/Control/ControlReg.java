84df7efad11e75c20edfe76baebd7ba64be40dca
AM <lgv2800@gmail.com>
2020-01-17 09:55:57 +0100

    Changed ControlReg. Added method getUserManagerHTTPClient.

--- src/main/java/ws18/Control/ControlReg.java
+++ src/main/java/ws18/Control/ControlReg.java
@@ -1,12 +1,14 @@
 package ws18.Control;
 import ws18.Database.*;
 import ws18.Exception.ExceptionContainer;
+import ws18.HTTPClients.UserManagerHTTPClient;
 import ws18.Service.*;
 
 public class ControlReg {
     private static ExceptionContainer exceptionContainer;
     private static IReportingService reportingService;
     private static IReportDatabase reportDatabase;
+    private static UserManagerHTTPClient userManagerHTTPClient;
 
     public static ExceptionContainer getExceptionContainer() {
         if (exceptionContainer == null) exceptionContainer = new ExceptionContainer();
@@ -21,5 +23,10 @@ public class ControlReg {
     public static IReportDatabase getReportDatabase() {
         if (reportDatabase == null) reportDatabase = new InMemoryReportDatabase();
         return reportDatabase;
+    }
+
+    public static UserManagerHTTPClient getUserManagerHTTPClient() {
+        if (userManagerHTTPClient == null) userManagerHTTPClient = new UserManagerHTTPClient();
+        return userManagerHTTPClient;
     }
 }
