d47c3a08110c0bafa84fa4b2fb8e95a55a175546
AM <lgv2800@gmail.com>
2020-01-17 13:49:11 +0100

    Added two HTTPClients: FastmoneyBank and PaymentClient.

---
+++ src/main/java/ws18/HTTPClients/FastmoneyBankHTTPClient.java
@@ -0,0 +1,26 @@
+package ws18.HTTPClients;
+
+import ws18.Model.Customer;
+import ws18.Model.DTUPayTransaction;
+import ws18.Model.DTUPayUser;
+import ws18.Model.Fastmoney.FastmoneyAccount;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class FastmoneyBankHTTPClient {
+
+    Client c = ClientBuilder.newClient();
+    WebTarget w = c.target("http://fastmoney-00.compute.dtu.dk/rest/accounts");
+
+    public String createAccount(FastmoneyAccount fastmoneyAccount) {
+        Response dtuPayTransactionResponse =
+                w.request().post(Entity.json(fastmoneyAccount));
+
+        return dtuPayTransactionResponse.toString();
+    }
+
+}
---
+++ src/main/java/ws18/HTTPClients/PaymentHTTPClient.java
@@ -0,0 +1,23 @@
+package ws18.HTTPClients;
+
+import ws18.Model.DTUPayTransaction;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+public class PaymentHTTPClient {
+
+    Client c = ClientBuilder.newClient();
+    WebTarget w = c.target("http://fastmoney-18.compute.dtu.dk:7474/payments");
+
+    public String saveDTUPayTransaction(DTUPayTransaction transaction) {
+        Response dtuPayTransactionResponse =
+                w.request().post(Entity.json(transaction));
+
+        return dtuPayTransactionResponse.toString();
+    }
+
+}
