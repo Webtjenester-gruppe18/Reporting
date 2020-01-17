f4da6ca409eeb9e3146739e7ffb1db2e1c7b3521
AM <lgv2800@gmail.com>
2020-01-17 13:49:33 +0100

    Changed ControlReg. Added FastmoneyBank and Payment client reference.

--- src/main/java/ws18/Control/ControlReg.java
+++ src/main/java/ws18/Control/ControlReg.java
@@ -1,6 +1,8 @@
 package ws18.Control;
 import ws18.Database.*;
 import ws18.Exception.ExceptionContainer;
+import ws18.HTTPClients.FastmoneyBankHTTPClient;
+import ws18.HTTPClients.PaymentHTTPClient;
 import ws18.HTTPClients.UserManagerHTTPClient;
 import ws18.Service.*;
 
@@ -9,6 +11,8 @@ public class ControlReg {
     private static IReportingService reportingService;
     private static IReportDatabase reportDatabase;
     private static UserManagerHTTPClient userManagerHTTPClient;
+    private static PaymentHTTPClient paymentHTTPClient;
+    private static FastmoneyBankHTTPClient fastMoneyBankHTTPClient;
 
     public static ExceptionContainer getExceptionContainer() {
         if (exceptionContainer == null) exceptionContainer = new ExceptionContainer();
@@ -28,5 +32,15 @@ public class ControlReg {
     public static UserManagerHTTPClient getUserManagerHTTPClient() {
         if (userManagerHTTPClient == null) userManagerHTTPClient = new UserManagerHTTPClient();
         return userManagerHTTPClient;
+    }
+
+    public static PaymentHTTPClient getPaymentHTTPClient() {
+        if (paymentHTTPClient == null) paymentHTTPClient = new PaymentHTTPClient();
+        return paymentHTTPClient;
+    }
+
+    public static FastmoneyBankHTTPClient getFastMoneyBankHTTPClient() {
+        if (fastMoneyBankHTTPClient == null) fastMoneyBankHTTPClient = new FastmoneyBankHTTPClient();
+        return fastMoneyBankHTTPClient;
     }
 }
