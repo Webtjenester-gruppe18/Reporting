74f435edcf171856a944dbfb329ad3c408453fce
AM <lgv2800@gmail.com>
2020-01-18 17:54:16 +0100

    Added ILocalReportingService and LocalReportingService.

---
+++ src/main/java/ws18/Service/ILocalReportingService.java
@@ -0,0 +1,17 @@
+package ws18.Service;
+
+import ws18.Model.*;
+
+import java.util.ArrayList;
+
+public interface ILocalReportingService {
+
+    DTUPayTransaction getTransactionById(String transactionId);
+    ArrayList<CustomerReportTransaction> getCustomerTransactionsByIds(Customer customer);
+    ArrayList<CustomerReportTransaction> getCustomerTransactionsByIdsFromThenToNow(Customer customer, long fromTime);
+    ArrayList<MerchantReportTransaction> getMerchantTransactionsByIds(Merchant merchant);
+    ArrayList<MerchantReportTransaction> getMerchantTransactionsByIdsFromThenToNow(Merchant merchant, long fromTime);
+    ArrayList<DTUPayTransaction> getAllTransactions();
+    String saveTransaction(DTUPayTransaction transaction);
+
+}
