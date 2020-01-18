9fdf580ae3cf6c11809d7493e9e487fbfb55a188
AM <lgv2800@gmail.com>
2020-01-18 18:06:36 +0100

    Refactoring. Removed FastmoneyHTTPClient and PaymentClient along with models.

--- src/main/java/ws18/Control/ControlReg.java
+++ src/main/java/ws18/Control/ControlReg.java
@@ -1,8 +1,6 @@
 package ws18.Control;
 import ws18.Database.*;
 import ws18.Exception.ExceptionContainer;
-import ws18.HTTPClients.FastmoneyBankHTTPClient;
-import ws18.HTTPClients.PaymentHTTPClient;
 import ws18.HTTPClients.UserManagerHTTPClient;
 import ws18.Service.*;
 
@@ -12,8 +10,6 @@ public class ControlReg {
     private static ILocalReportingService localReportingService;
     private static IReportDatabase reportDatabase;
     private static UserManagerHTTPClient userManagerHTTPClient;
-    private static PaymentHTTPClient paymentHTTPClient;
-    private static FastmoneyBankHTTPClient fastMoneyBankHTTPClient;
 
     public static ExceptionContainer getExceptionContainer() {
         if (exceptionContainer == null) exceptionContainer = new ExceptionContainer();
@@ -40,13 +36,4 @@ public class ControlReg {
         return userManagerHTTPClient;
     }
 
-    public static PaymentHTTPClient getPaymentHTTPClient() {
-        if (paymentHTTPClient == null) paymentHTTPClient = new PaymentHTTPClient();
-        return paymentHTTPClient;
-    }
-
-    public static FastmoneyBankHTTPClient getFastMoneyBankHTTPClient() {
-        if (fastMoneyBankHTTPClient == null) fastMoneyBankHTTPClient = new FastmoneyBankHTTPClient();
-        return fastMoneyBankHTTPClient;
-    }
 }
