87545ad43d67f65ff25381027d38afb3cf6c3007
AM <lgv2800@gmail.com>
2020-01-21 12:24:34 +0100

    Changed ReportingSteps. Test now passes.

--- src/test/java/ws18/Reporting/ReportingSteps.java
+++ src/test/java/ws18/Reporting/ReportingSteps.java
@@ -2,55 +2,55 @@ package ws18.Reporting;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.mockito.ArgumentCaptor;
 import ws18.messagingutils.IEventSender;
-import ws18.model.Customer;
-import ws18.model.CustomerReportTransaction;
-import ws18.model.Merchant;
-import ws18.model.MerchantReportTransaction;
+import ws18.model.*;
+import ws18.service.ReportingService;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class ReportingSteps {
 
-    private Customer currentCustomer;
-    private Merchant currentMerchant;
-    private ArrayList<CustomerReportTransaction> customerTransactions;
-    private ArrayList<MerchantReportTransaction> merchantReportTransactions;
+    private ArrayList<DTUPayTransaction> dtuPayTransactions = new ArrayList<>();
     private IEventSender eventSender = mock(IEventSender.class);
-    //private ILocalReportingService localReportingService = new LocalReportingService(eventSender);
-    private String errormessage = "";
+    private ReportingService reportingService = new ReportingService(eventSender);
 
     @When("the service receives the {string} event")
-    public void theServiceReceivesTheEvent(String string) {
-        /*
+    public void theServiceReceivesTheEvent(String string) throws Exception {
         Event event = new Event();
         event.setType(EventType.valueOf(string));
-        if (event.getType().equals(EventType.REQUEST_FOR_NEW_TOKENS)||event.getType().equals(EventType.RETRIEVE_TOKENS)) {
-            event.setObject("123");
-        }else if(event.getType().equals(EventType.TOKEN_VALIDATION_REQUEST)){
-            Token token = tokenManager.generateToken("456");
-            PaymentRequest request= new PaymentRequest();
-            request.setAmount(BigDecimal.ONE);
-            request.setCpr("456");
-            request.setDescription("Test");
-            request.setToken(token);
-            event.setObject(request);
+        if (event.getType().equals(EventType.REQUEST_TRANSACTIONS_RESPONSE)) {
+
+            DTUPayTransaction transaction = new DTUPayTransaction(
+                    BigDecimal.valueOf(100),
+                    "000000-0000",
+                    "111111-1111",
+                    "Test",
+                    new Date().getTime(),
+                    new Token()
+            );
+
+            dtuPayTransactions.add(transaction);
+
+            event.setObject(dtuPayTransactions);
         }
 
-        tokenManager.receiveEvent(event);*/
+        reportingService.receiveEvent(event);
     }
 
     @Then("the report is created")
     public void theReportIsCreated() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
     }
 
     @Then("the {string} event is broadcast")
-    public void theEventIsBroadcast(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void theEventIsBroadcast(String string) throws Exception {
+        ArgumentCaptor<Event> argumentCaptor = ArgumentCaptor.forClass(Event.class);
+        verify(eventSender, atLeastOnce()).sendEvent(argumentCaptor.capture());
+        Assert.assertEquals(EventType.valueOf(string), argumentCaptor.getValue().getType());
     }
 }
